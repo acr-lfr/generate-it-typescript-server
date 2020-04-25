248c77692c6d46f18932e57cab6e04bb9ca6fd4b
John Carmichael <john@johnine.org>
2020-04-25 13:21:31 +0200

    feat: worker logic bundled into the tpl files

--- src/config.ts
+++ src/config.ts
@@ -25,5 +25,21 @@ export default {
 
   // Api key
   apiKey: ConfigHelper.withDefault('API_KEY', false),
+
+  // Request worker config - allThreadsCount = processes * threadsPerProcess
+  requestWorker: {
+    processes: Number.parseInt(
+      ConfigHelper.withDefault('REQUEST_WORKER_PROCESSES', 1),
+      10
+    ),
+    threadsPerProcess: Number.parseInt(
+      ConfigHelper.withDefault('REQUEST_WORKER_THREADS_PER_PROCESS', 10),
+      10
+    ),
+    timeoutMs: Number.parseInt(
+      ConfigHelper.withDefault('REQUEST_WORKER_TIMEOUT_MS', 300000), // 5 minutes
+      10
+    )
+  }
 };
 /* tslint:enable */
