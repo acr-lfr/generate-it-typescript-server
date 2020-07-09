caa3b0ffd7928391277d5add0588a0558cf0eb4b
John <john@johnine.org>
2020-07-09 12:59:20 +0200

    feat: app returns a promise for the cli and server ts to know when ready

--- server.ts
+++ server.ts
@@ -3,17 +3,23 @@ import run from '@/cli/run';
 import app from './src/app';
 import appCli from './src/app.cli';
 import config from './src/config';
-import packageJson from './package.json'
+import packageJson from './package.json';
 
 const cliInput = appCli();
 const PORT = cliInput.port || config.port;
 
-if (cliInput['run-script']) {
-  // call the script runner
-  run(cliInput['run-script']);
-} else {
-  // Start listening for incoming HTTP requests
-  app.listen(PORT, () => {
-    console.log(`${packageJson.name}:${packageJson.version} server listening on port, ${PORT}`);
-  });
-}
+app()
+  .then((expressApp) => {
+    if (cliInput['run-script']) {
+      // call the script runner
+      run(cliInput['run-script']);
+    } else {
+      // Start listening for incoming HTTP requests
+      expressApp.listen(PORT, () => {
+        console.log(`${packageJson.name}:${packageJson.version} server listening on port, ${PORT}`);
+      });
+    }
+  }).catch((e) => {
+  console.error('Error calling the app:');
+  throw e;
+});
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
-import express from 'express';
+import express from 'express'
+import middlewareErrorHandling from './http/middlewareErrorHandling'
+import middlewaresImporter from './http/middlewaresImporter'
+import routesImporter from './http/nodegen/routesImporter'
 
-import middlewareErrorHandling from './http/middlewareErrorHandling';
-import middlewaresImporter from './http/middlewaresImporter';
-import routesImporter from './http/nodegen/routesImporter';
-
-const app = express();
-
-middlewaresImporter(app);
-routesImporter(app);
-middlewareErrorHandling(app);
-
-export default app;
+/**
+ * Returns a promise allowing the server or cli script to know
+ * when the app is ready; eg database connections established
+ */
+export default (): Promise<express.Express> => {
+  return new Promise((resolve, reject) => {
+    // Here is a good place to connect to databases if required,
+    // resolve once connected else reject
+    const app = express()
+    middlewaresImporter(app)
+    routesImporter(app)
+    middlewareErrorHandling(app)
+    return resolve(app)
+  })
+}
