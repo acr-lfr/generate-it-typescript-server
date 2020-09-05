e5d865990796cdc326c5ec80807f8799b2eab15a
John Carmichael <john@johnine.org>
2020-09-05 14:50:24 +0200

    feat: async functions tested and now working

--- src/services/AsyncValidationService.ts
+++ src/services/AsyncValidationService.ts
@@ -6,27 +6,31 @@ class AsyncValidationService {
    * Entry function for the src/http/nodegen/middleware/asyncValidationMiddleware.ts
    */
   middleware (req: NodegenRequest, res: express.Response, next: express.NextFunction, asyncValidators: string[]) {
+    this.parseValidators(req, asyncValidators).then(() => {
+      next();
+    }).catch((e) => {
+      throw e;
+    });
+  }
+
+  async parseValidators (req: NodegenRequest, asyncValidators: string[]) {
     for (let i = 0; i < asyncValidators.length; ++i) {
       const asyncValidatorParts = asyncValidators[i].split(':');
-      const methodToCall = String(asyncValidatorParts.shift());
-      // It is expected the custom async validation method will throw its own http errors
+      // Expected the 1st element is the function name followed by params for the given given function
       // @ts-ignore
-      this[methodToCall](req, asyncValidatorParts).then(() => {
-        next();
-      }).catch((e: any) => {
-        throw e;
-      });
+      await this[asyncValidatorParts.shift()](req, asyncValidatorParts);
     }
   }
 
   /**
-   * EXMAPLE ONLY
+   * EXAMPLE ONLY
    * @param req
    * @param asyncValidatorParams
    */
   async uniqueUsername (req: NodegenRequest, asyncValidatorParams: string[]): Promise<void> {
     /**
-     * const user = db.user.find({ username: req.body.username })
+     * // Run the async function and throw the required error when needed
+     * const user = await db.user.find({ username: req.body.username })
      * if(user){
      *   throw http422()
      * }
