12a29a3d10cfd9a766bb56a4e4d4355e1d818676
John Carmichael <john@johnine.org>
2020-10-03 20:10:54 +0200

    WIP: breadcrumbs from the type to the middleware

--- src/http/nodegen/interfaces/GenerateItExpressResponse.ts
+++ src/http/nodegen/interfaces/GenerateItExpressResponse.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
 export default interface GenerateItExpressResponse extends express.Response {
+  // src/http/nodegen/middleware/inferResponseType.ts
   inferResponseType: (
     dataOrPath: any,
     status: number,
