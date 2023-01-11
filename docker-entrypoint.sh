ac055c2734482b92c804161baaccb45f148af8c2
Santiago Sanchez <ssd170@gmail.com>
2023-01-11 15:48:14 +0100

    small updates for node 18 and update packages with vulnerabilities

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,14 +10,17 @@ command="${1:-prod}"
 
 case $command in
   watch)
+    NODE_ENV=development
     npm install
     npm run dev:build-watch
   ;;
   dev)
+    NODE_ENV=development
     echo "Running dev server..."
-    npm run dev:start-watch
+    npm run start
   ;;
   prod)
+    NODE_ENV=production
     echo "Running server..."
     npm run start
   ;;
