85bb79f6971af9d790bcc032b4b5c6a111713247
p-mcgowan@winbot <mcgowanapat+git@gmail.com>
2025-10-17 22:55:21 +0200

    fix: content-negotiation
    - resolve REDOS issues, swap for simple splits / trims
    - fix wildcard */* (the temp fix actually just always returned
      produces[0] and not the negotiated type)
    - add arbitray 1024 accept header max length for DOS mitigation

--- src/http/nodegen/utils/getPreferredResponseFormat.ts
+++ src/http/nodegen/utils/getPreferredResponseFormat.ts
@@ -1,61 +1,70 @@
+import { NotAcceptableException } from '@/http/nodegen/errors';
+
 /**
  * Figure out which response type to send based on accept header
  * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept
  *
  * @param  {string}  accept       The accept header
- * @param  {array}   mimes        Our desired response types (in order as defined in the openapi file)
+ * @param  {array}   produces     Our available response types (in order as defined in the openapi file)
  *
  * @return {string | undefined}   If the accept header contains something we would like to
  *                                send, it will be returned, else undefined.
  */
-export default (accept: string, mimes: string[]): string | undefined => {
-  if (!accept || !mimes?.length) {
+export default (accept: string, produces: string[]): string | undefined => {
+  produces = produces.filter(Boolean);
+  if (!accept || !produces?.length) {
     return;
   }
 
-  // escape all special chars except *
-  const formatRegex = (s: string) => s.replace(/[.+?^${}()|[\]\\]/g, '\\$&')
-
-  const priority: string[][] = accept.split(/\s*,\s*/).reduce((acc: string[][], val) => {
-    const [mime, ...extra] = val.split(/\s*;\s*/);
-
-    // extension might look like { q: '0.1', charset: 'utf-8' }
-    // https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1
-    const extension = extra.reduce((acc: Record<string, string>, val) => {
-      const [key, value] = val.split('=').map(s => s.trim());
-      acc[key] = value;
-
-      return acc;
-    }, {});
-
-    const prioValue = (extension.p || '1').replace(/.*=\s*/, '');
-    const index = 10 - Math.round(parseFloat(prioValue) * 10);
-    acc[index] = (acc[index] || []).concat(mime);
-
-    return acc;
-  }, []);
-
-  const parts = [...mimes, '*/*'].reduce((acc: string[], mime) => {
-    if (!mime) {
-      return acc;
-    }
-    const [type, subtype] = mime.split('/').map(formatRegex);
-    if (!subtype) {
-      return acc.concat(type.replace(/\*/g, '\\*'));
-    }
-
-    return acc.concat(`${type}\/(${subtype}|*)`.replace(/\*/g, '\\*'));
-  }, []);
-
-  const willAccept = new RegExp(parts.join('|'));
-  const matchingAccept = priority.find((mimeTypes) => mimeTypes.find((mime) => willAccept.test(mime)))?.[0];
-
-  if (!matchingAccept) {
-    return;
+  // DOS mitigation - could be safely bumped higher
+  const maxHeaderLength = 1024;
+  if (accept.length > maxHeaderLength) {
+    throw new NotAcceptableException(`Accept header too large. Max length is ${maxHeaderLength}`);
   }
 
+  // this could be moved out since it only needs to be evaluated once per route not every request
+  const producable: Record<string, string[]> = {};
+  for (const mime of produces) {
+    (producable[mime] ||= []).push(mime);
+    (producable['*/*'] ||= []).push(mime);
+    const main = mime.split('/')[0].trim();
+    (producable[main] ||= []).push(mime);
+  }
 
-  const matchRegex = new RegExp(matchingAccept.replace(/[.+?^${}()|[\]\\]/g, '\\$&').replace(/\*/g, '[^/]*'));
+  const prioritizedAccept: { full: string; main: string; sub: string; q: number }[] = [];
+  for (const option of accept.split(',')) {
+    const [mime, ...extras] = option.split(';').map((x) => x.trim());
 
-  return mimes.find((mime) => matchRegex.test(mime));
+    const [main, sub] = mime.split('/');
+    const acceptOption = { full: mime, main, sub, q: 1 };
+
+    for (const extra of extras) {
+      const [key, value] = extra.split('=').map((x) => x.trim());
+      // basic impl: just ignore every other option besides priority (eg charset, lang)
+      if (key === 'q') {
+        const val = parseFloat(value);
+        if (!isNaN(val)) {
+          acceptOption.q = val;
+        } else {
+          // could throw 406 but we just de-prio it if it's malformed
+          acceptOption.q = 0;
+        }
+        break;
+      }
+    }
+    prioritizedAccept.push(acceptOption);
+  }
+
+  // for same prio, length of main + sub is a good enough proxy for specificity taking preference
+  // https://httpwg.org/specs/rfc9110.html#field.accept "Media ranges can be overridden by more specific media ranges..."
+  prioritizedAccept.sort((a, b) => b.q - a.q || (b.main + b.sub).length - (a.main + a.sub).length);
+
+  for (const acceptable of prioritizedAccept) {
+    if (acceptable.full in producable) {
+      return producable[acceptable.full][0];
+    }
+    if (acceptable.sub === '*' && acceptable.main in producable) {
+      return producable[acceptable.main][0];
+    }
+  }
 };
